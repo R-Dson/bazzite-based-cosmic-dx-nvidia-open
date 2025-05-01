f62c6ce32831b5b876e586067d22468241a7efe5
Saltaformajo <corp@dev.null>
2025-05-02 00:51:26 +0200

    Added cosmic packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# Tagged release
+dnf5 install -y @cosmic-desktop-environment
+
+# Nightly release
+# dnf5 copr enable ryanabx/cosmic-epoch
+# dnf5 install cosmic-desktop
 
 # Use a COPR Example:
 #
